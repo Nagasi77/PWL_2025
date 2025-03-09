38264ba4821cadd176ccf88b803c2710b3a0dad3
Much. Zannur Nadhif <muchzannurnadhif@gmail.com>
2025-03-09 17:42:27 +0700

    Jobsheet5praktikum2(half)

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -1,15 +1,20 @@
 <section class="content-header">
-    <div class="container-fluid">
+  <div class="container-fluid">
       <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Blank Page</h1>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Blank Page</li>
-          </ol>
-        </div>
+          <div class="col-sm-6">
+              <h1>{{ $breadcrumbs->title }}</h1>
+          </div>
+          <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                  @foreach($breadcrumbs->list as $key => $value)
+                      @if($key == count($breadcrumbs->list) - 1)
+                          <li class="breadcrumb-item active">{{ $value }}</li>
+                      @else
+                          <li class="breadcrumb-item">{{ $value }}</a></li>
+                      @endif
+                  @endforeach
+              </ol>
+          </div>
       </div>
-    </div><!-- /.container-fluid -->
-  </section>
+  </div>
+</section>
