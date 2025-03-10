fe688852071378d9cbb7b8c78ec218700cb9e6b5
Much. Zannur Nadhif <muchzannurnadhif@gmail.com>
2025-03-10 09:37:59 +0700

    Jobsheet5praktikum3

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
-
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\kategoriController;
 use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WelcomeController::class, 'index']);
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-
-Route::get('/home', HomeController::class);
-Route::get('user/tambah', [UserController::class, 'tambah']);
-// Prefix untuk kategori produk
-Route::prefix('category')->group(function () {
-    Route::get('/{type}', [ProductController::class, 'category']);
-});
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-// Parameter untuk user
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::get('/sales', SalesController::class);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -38,6 +15,15 @@ Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 |
 */
 
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/', function () {
-});
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+}); 
